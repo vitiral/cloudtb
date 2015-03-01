eac82fb9fd4586e62670787a0521615be9f99000
Garrett Berg <gberg@neutral.io>
2015-03-01 13:02:50 -0700

    re: updated to new object, updated tests. Added str

--- cloudtb/re.py
+++ cloudtb/re.py
@@ -11,34 +11,12 @@ def groups(searched):
     return tuple(group(i) for i in range(len(searched.regs)))
 
 
-def research(exp, text, start=0, end=None, parseregex=True):
-    '''Similar to re.search except it returns a tuple of Group objects.
-    These are more interactive and more easy to glean data from.'''
-    if isinstance(exp, (str, bytes)):
-        exp = re.compile(exp)
-    end = len(text) if end is None else end
-    search = exp.search
-    pos = start
-    count = 0
-    prev_stop = pos
-    while pos < end:
-        searched = search(text, pos, end)
-        if searched is None:
-            break
-        count += 1
-        if count > end:
-            assert False
-        start, stop = searched.span()
-        # get the raw text (no match)
-        t = text[prev_stop: start]
-        if t is not '':
-            yield t
-        if start == stop:  # empty match
-            prev_stop += 1
-            continue
+class research(tuple):
+    def __new__(cls, exp, text, start=0, end=None):
+        return tuple.__new__(cls, _research(exp, text, start, end))
 
-        yield Group(searched, groups(searched))
-        prev_stop = stop
+    def __repr__(self):
+        return ''.join(str(n) for n in self)
 
 
 class Group:
@@ -48,46 +26,78 @@ class Group:
         index -- get the group index
     '''
     def __init__(self, text, searched, groups, index=0):
-        indexes = []
-        text = groups[index]
+        indexes = [index]
+        mytext = groups[index]
         matches = []
         myreg = searched.regs[index]
         mystart, myend = myreg
-        prev_end = 0 if index is 0 else searched.regs[index][1]
+        # prev_end = 0 if index is 0 else searched.regs[index][1]
+        prev_end = mystart
+        index += 1
         while index < len(searched.regs):
             reg = searched.regs[index]
             start, end = reg
-            if reg == myreg:
-                indexes.append(index)
-                index += 1
-                continue
-            elif start >= myend and end > myend:
+            if start >= myend and end > myend:
                 # the reg does not fit in self
                 break
             assert start >= mystart and end <= myend
             if prev_end < start:
                 # store raw text
                 matches.append(text[prev_end:start])
+            if reg == myreg:
+                indexes.append(index)
+                index += 1
+                continue
             matches.append(Group(text, searched, groups, index))
             index += len(matches[-1].indexes)
             prev_end = searched.regs[index - 1][1]
 
-        self.text = text
+        self.text = mytext
         self.reg = myreg
         self.matches = matches
         self.indexes = indexes
 
     @property
     def index(self):
-        '''Simplified index. Technically the match can be multiple
-        indexes because it can contain embeded groups'''
+        '''Simplified index number
+
+        Note: one regexp can be multiple group indexes
+        '''
         return self.indexes[0]
 
-    def __str__(self):
-        start, end = '', ''
+    def __repr__(self):
+        start, end = '[', '#{}]'.format(self.index)
         if self.matches:
-            match = self.matches[0]
-            start = '<*m{0}>[['.format(match)
-            end = r']]'
-        str_data = ''.join([str(n) for n in self.matches])
-        return start + '{{{0}}}<g{1}>'.format(str_data, self.indexes) + end
+            middle = ''.join(str(n) for n in self.matches)
+        else:
+            middle = self.text
+        return start + middle + end
+
+
+def _research(exp, text, start=0, end=None):
+    if isinstance(exp, (str, bytes)):
+        exp = re.compile(exp)
+    end = len(text) if end is None else end
+    search = exp.search
+    pos = start
+    count = 0
+    # import ipdb; ipdb.set_trace()
+    while pos <= end:
+        searched = search(text, pos, end)
+        if searched is None:
+            break
+        count += 1
+        if count > end:
+            assert False
+        start, stop = searched.span()
+        # get the raw text (no match)
+        t = text[pos: start]
+        if t is not '':
+            yield t
+        if start == stop:  # empty match
+            pos += 1
+            continue
+
+        yield Group(text, searched, groups(searched))
+        pos = stop
+    yield text[pos:]
--- tests/test_re.py
+++ tests/test_re.py
@@ -30,26 +30,45 @@ class TestGroup(TestCase):
         searched = re.search(exp, text)
         grps = groups(searched)
         self.assertEqual(grps, (text, 'foo bar', 'bar', 'foo bar'))
+        # import ipdb; ipdb.set_trace()
         group = Group(text, searched, grps)
         matches = group.matches
         self.assertEqual(matches[0].text, 'foo bar')
         self.assertEqual(matches[0].indexes, [1])
-        self.assertEqual(matches[0].matches[0].text, 'bar')
-        self.assertEqual(matches[0].matches[0].indexes, [2])
+        self.assertEqual(matches[0].matches[0], 'foo ')
+        self.assertEqual(matches[0].matches[1].text, 'bar')
+        self.assertEqual(matches[0].matches[1].indexes, [2])
         self.assertEqual(matches[1].text, 'bar')
         self.assertEqual(matches[1].indexes, [2])
         self.assertEqual(matches[2], ' is grouped differently than ')
         self.assertEqual(matches[3].text, 'foo bar')
         self.assertEqual(matches[3].indexes, [3])
 
+    def test_str(self):
+        exp = '(foo (bar)).*(foo bar)'
+        text = 'foo bar is grouped differently than foo bar'
+        searched = re.search(exp, text)
+        group = Group(text, searched, groups(searched))
+        expected = ("[[foo [bar#2]#1][bar#2] is grouped "
+                    "differently than [foo bar#3]#0]")
+        self.assertEqual(str(group), expected)
 
-def TestResearch(TestCase):
+
+class TestResearch(TestCase):
     def test_basic(self):
-        exp = '(foo).*(bar)'
+        exp = '(foo).*?(bar)'
         text = 'so foo is the opposite of bar but without foo there is no bar?'
-        searched = tuple(research(exp, text))
+        searched = research(exp, text)
         self.assertEqual(searched[0], 'so ')
         self.assertEqual(searched[1].text, 'foo is the opposite of bar')
         self.assertEqual(searched[2], ' but without ')
         self.assertEqual(searched[3].text, 'foo there is no bar')
         self.assertEqual(searched[4], '?')
+
+    def test_str(self):
+        exp = '(foo).*(bar)'
+        text = 'so foo is the opposite of bar but without foo there is no bar?'
+        searched = research(exp, text)
+        expected = ('so [[foo#1] is the opposite of bar but without foo there '
+                    'is no [bar#2]#0]?')
+        self.assertEqual(str(searched), expected)
