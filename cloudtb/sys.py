ba73abecf8b4d2eae0f635b37c8f08e7da389818
Garrett Berg <gberg@neutral.io>
2015-03-02 15:58:59 -0700

    sys: forgot sys modules for color access

---
+++ cloudtb/sys.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+def supports_color():
+    """Returns True if the running system's terminal supports color,
+    and False otherwise.
+    based on django.core.management.color
+    """
+    plat = sys.platform
+    if plat == 'Pocket PC' or (plat == 'win32' and 'ANSICON' not in os.environ):
+        return False
+    if not hasattr(sys.stdout, 'isatty') or not sys.stdout.isatty():
+        return False
+    return True
+
+
+hascolor = supports_color()  # fast access
