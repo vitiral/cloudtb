e5b6d1741cc5b3cd36157cc8bc8f43356a1f22a6
Garrett Berg <gberg@neutral.io>
2015-03-04 11:18:33 -0700

    os.abspath: added tests

--- tests/test_os.py
+++ tests/test_os.py
@@ -1,7 +1,23 @@
 
 from unittest import TestCase
-from cloudtb.os import split
+from cloudtb.os import split, abspath
 import os
+
+
+class TestAbspath(TestCase):
+    def test_nouser(self):
+        '''when there is no user, the added feature has no impact'''
+        plist = ['', 'foo', 'bar', 'zaz']
+        expected = os.path.sep.join(plist)
+        result = abspath(expected)
+        self.assertEqual(expected, result)
+
+    def test_nouser_nonroot(self):
+        plist = ['foo', 'bar', 'zaz']
+        path = os.path.sep.join(plist)
+        expected = os.path.abspath(path)
+        result = abspath(expected)
+        self.assertEqual(expected, result)
 
 
 class TestSplit(TestCase):
