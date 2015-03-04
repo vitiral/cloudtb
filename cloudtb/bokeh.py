f9318714990e29b9501e6c1385d603d74fa75877
Garrett Berg <gberg@neutral.io>
2015-03-04 00:45:49 -0700

    bokeh, plot: moving functions to general plot library, added documentation. Need to test and add tests

--- cloudtb/bokeh.py
+++ cloudtb/bokeh.py
@@ -13,126 +13,13 @@ import numpy as np
 import pandas as pd
 from bokeh.models import LinearAxis, Range1d
 from bokeh.palettes import brewer
-from scipy import stats
-import colorsys
 
-
-def get_hex(color):
-    color = tuple(int(n * 255) for n in color)
-    return ('#' + '{:02x}' * 3).format(*color)
-
-
-def get_colors(num_colors, hex=False):
-    colors = []
-    for i in np.arange(0., 360., 360. / num_colors):
-        hue = i / 360
-        lightness = (50 + np.random.rand() * 10) / 100
-        saturation = (90 + np.random.rand() * 10) / 100
-        color = colorsys.hls_to_rgb(hue, lightness, saturation)
-        if hex:
-            color = get_hex(color)
-        colors.append(color)
-    return colors
-
-
-def get_bins(x, y, bins):
-    '''Put data into bins'''
-    prev = 0
-    assert len(x) == len(y)
-    if len(y) <= bins:
-        yield x, y
-        raise StopIteration
-    for i in range(len(y) // bins, len(y) - 1, len(y) // bins):
-        yx, yy = x[prev:i], y[prev:i]
-        assert len(yx) == len(yy)
-        yield yx, yy
-        prev = i
-    if prev < len(x) - 1:
-        yx, yy = x[prev:], y[prev:]
-        assert len(yx) == len(yy)
-        yield yx, yy
-
-
-def get_time_bins(series, window):
-    '''Returns data in time bins (in seconds).
-    Bins must be on the index of the series'''
-    start = min(series.index)
-    end = max(series.index)
-    prev = start
-    print("Times:", start, end, window)
-    for t in np.arange(start, end, window):
-        yield series[prev:t]
-        prev = t
-    yield series[prev:]
-
-
-def time_slopes(series, window):
-    series = series.dropna()
-    mx, my = [], []
-    for s in get_time_bins(series, window):
-        bx, by = s.index, s.values
-        # print(bx, by)
-        m, c, r_value, p_value, std_err = stats.linregress(bx, by)
-        mx.append(np.mean(bx))
-        my.append(m)
-    return mx, my
-
-
-def slopes(series, perbin=10):
-    series = series.dropna()
-    x, y = series.index, series.values
-    bins = len(series) // perbin
-    data = []
-    mx = []
-    for bx, by in get_bins(x, y, bins):
-        m, c, r_value, p_value, std_err = stats.linregress(bx, by)
-        data.append(m)
-        mx.append(np.mean(bx))
-    return mx, data
-
-
-def linear_fit(x, y, bins=None):
-    '''Return the linear variability in a number of bins'''
-    if bins is None:
-        # A = np.vstack([x, np.ones(len(x))]).T
-        # m, c = np.linalg.lstsq(A, y)[0]
-        m, c, r_value, p_value, std_err = stats.linregress(x, y)
-        return m * x + c
-    else:
-        data = []
-        for bx, by in get_bins(x, y, bins):
-            data.append(linear_fit(bx, by))
-        return np.concatenate(data)
-
-
-def mean_fit(x, y, bins=None):
-    if bins is None:
-        return np.mean(x), np.mean(y)
-    else:
-        dx = []
-        dy = []
-        for bx, by in get_bins(x, y, bins):
-            bx, by = mean_fit(bx, by)
-            dx.append(bx)
-            dy.append(by)
-        return np.array(dx), np.array(dy)
-
-
-def linear_std(x, y, bins, line=None):
-    '''Returns the normalized standard deviation of the data matched to *bins* lines'''
-    # get unit mean
-    unit_mean = np.mean(y - np.min(y))
-    # normalize it to be around the center
-    if line is None:
-        line = linear_fit(x, y, bins)
-    data = line - y
-    # return the normalized linear standard deviation
-    return np.std(data) / unit_mean
+from cloudtb import plot
 
 
 def whisker(fig, x, y, bins=None, line_width=1, color='blue', name=None, **kwargs):
     means = []
-    for bx, by in get_bins(x, y, bins):
+    for bx, by in plot.get_bins(x, y, bins):
         # find the quantiles
         vlower, low, mid, up, vupper = np.percentile(by, [10, 25, 50, 75, 90])
         xpoint = np.mean(bx)
@@ -156,11 +43,10 @@ def whisker(fig, x, y, bins=None, line_width=1, color='blue', name=None, **kwarg
 def scatter(fig, x, y, bins, color='blue', name=None, **kwargs):
     '''A friendly scatter plot'''
     opaque = 0.4
-    drange = max(y) - min(y)
     if len(y) <= bins:
         return fig.line(x, y, color=color, legend=name,
                         line_width=1, **kwargs)
-    linex, liney = mean_fit(x, y, bins)
+    linex, liney = plot.mean_fit(x, y, bins)
     fig.circle(x, y, radius=1, fill_alpha=opaque, line_alpha=opaque,
                line_color=color, fill_color=color, legend=name, **kwargs)
     return fig.line(linex, liney, line_color=color,
@@ -194,7 +80,7 @@ def get_axis(fig, df):
     except KeyError:
         # std_colors = tuple(zip(np.linspace(100, 255, glen + 1, dtype=int),
         #                  np.zeros(glen, dtype=int), np.zeros(glen, dtype=int)))
-        std_colors = get_colors(glen, hex=True)
+        std_colors = plot.get_colors(glen, hex=True)
         std_colors = iter(std_colors)
     if not small.empty:
         fig.yaxis.axis_line_color = 'red'
@@ -206,13 +92,19 @@ def get_axis(fig, df):
                   else None for key in df.columns}
 
 
-def dataframe(fig, df,
-              variability_cutoff: "data with variabilty > this will be box-whisker plot"=0.15,
-              min_bins: "The minimum number of bins"=10,
-              bins: "the desired number of bins"=30,
-              num_per_bin: "dataframe will try to have at least this many data points per bin"=20,
-              min_num_per_bin: "any number less than this will just be plotted as a scatter plot"=5,
+def dataframe(fig, df, variability_cutoff=0.15,
+              min_bins=10, bins=30, num_per_bin=20, min_num_per_bin=5,
               color='blue'):
+    '''
+    Arguments:
+        variability -- data with variabilty > this will be box-whisker plot
+        min_bins -- the minimum number of bins
+        bins -- the desired number of bins
+        num_per_bin -- dataframe will try to have at least this many
+            data points per bin
+        min_num_per_bin -- any number less than this will just be plotted as
+            a scatter plot
+    '''
     axis = get_axis(fig, df)
     for col in df.columns:
         scat = False
@@ -224,8 +116,8 @@ def dataframe(fig, df,
         if len(data) / min_bins < min_num_per_bin:
             scat = True
         if not scat:
-            line = linear_fit(data.index, data.values, _bins)
-            std = linear_std(data.index, data.values, _bins, line=line)
+            line = plot.linear_fit(data.index, data.values, _bins)
+            std = plot.linear_std(data.index, data.values, _bins, line=line)
             if std <= variability_cutoff:
                 scat = True
         yname = axis[col]
